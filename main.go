1ae5a6e93cb5f49ffd9064f6f1fb425b37cb1664
hazi-tgi <mohamedhaseeb@ateamsoftsolutions.com>
2024-06-20 23:38:13 +0530

    env & cors setup done

--- main.go
+++ main.go
@@ -2,42 +2,34 @@ package main
 
 import (
 	"log"
-	// "time"
+	"os"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 
 	// "github.com/hazitgi/go_gin_server/apis"
 	"github.com/hazitgi/go_gin_server/database"
 	"github.com/hazitgi/go_gin_server/handlers"
 	"github.com/hazitgi/go_gin_server/managers"
-	// "github.com/hazitgi/go_gin_server/middleware"
 	"github.com/gin-contrib/static"
 )
 
 func main() {
 	// Create a gin router
-	gin.SetMode(gin.DebugMode)
 	router := gin.Default()
-	// router.Use(middleware.ConfigureCORSMiddleware())
-	// init CORS middleware
-	router.Use(cors.Default())
-	// router.Use(cors.New(cors.Config{
-	//     AllowOrigins:     []string{"*"}, // Replace with your frontend URL
-	//     AllowMethods:     []string{"GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD"},
-	//     AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
-	//     ExposeHeaders:    []string{"Content-Length"},
-	//     AllowCredentials: true,
-	//     MaxAge:           12 * time.Hour,
-	// }))
 
-	// router.OPTIONS("/", func(c *gin.Context) {
-	// 	c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
-	// 	c.Header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
-	// 	c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization")
-	// 	c.Header("Access-Control-Allow-Credentials", "true")
-	// 	c.JSON(200, gin.H{"message": "OPTIONS request handled"})
-	// })
+	_ = godotenv.Load(".env")
+	env := os.Getenv("ENV")
+	if env == "development" {
+		gin.SetMode(gin.DebugMode)
+		router.Use(cors.Default())
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+		config := cors.DefaultConfig()
+		config.AllowOrigins = []string{"https://hazitgi.github.io/", "http://localhost:8000"}
+		router.Use(cors.Default())
+	}
 
 	// initioalize database
 	initDB()
